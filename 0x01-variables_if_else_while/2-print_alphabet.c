1475293b16eb1632253e4b32f02054c28e863b7e
Roylouis-web <powellleroy488@gmail.com>
2022-09-09 14:56:28 +0000

    My third task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main-Entry point
+*Return:Always 0 (Success)
+*/
+int main(void)
+{
+char count = 'a';
+while (count <= 'z')
+{putchar(count);
+count++;
+}
+return (0);
+}
