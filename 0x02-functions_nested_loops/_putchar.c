66972628c89b5c0c07bfced2d6da948f9b915822
Roylouis-web <powellleroy488@gmail.com>
2022-09-13 12:10:39 +0000

    My first task

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * _putchar - Entry point
+ * Description: contains the function to be used in the main function.
+ * @charac: parameter for the required output.
+ * Return: Always Successful
+ */
+char _putchar(char charac)
+{
+return (putchar(charac));
+}
