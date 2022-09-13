af8b4e1aabab6d3924e67907aaa87578187686ea
Roylouis-web <powellleroy488@gmail.com>
2022-09-13 17:03:18 +0000

    My eleventh task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add -adds two integers and returns the result.
+*@a: integer parameter to be used in the function below
+*@b:second integer parameter to be used in the function below
+*Return: Always Successful
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
+
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,9 @@
+/**
+ * print_last_digit - prints the last digit of a number.
+ * @c:integer parameter for the function below.
+ * Return: Always Successful
+ */
+int print_last_digit(int c)
+{
+
+}
