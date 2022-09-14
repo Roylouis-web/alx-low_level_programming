abdeedbe989409de555364395c522edba93893ee
Roylouis-web <powellleroy488@gmail.com>
2022-09-14 03:58:38 +0000

    My fourteenth task

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * main - prints the sum of all the multiples of 3 or 5
- * int : Returns nothing
- *
- * Returns:Always Successful
+ *main - prints the sum of all the multiples of 3 or 5
+ *Return: Always 0 (Success)
  */
 int main(void)
 {int sum = 0;
-int nums = 1;
+int nums = 0;
 while (nums < 1024)
 {
 if (nums % 3 == 0 || nums % 5 == 0)
