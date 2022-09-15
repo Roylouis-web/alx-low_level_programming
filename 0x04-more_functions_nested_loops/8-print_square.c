b774a3d09ac9626df43dd27b8e0af1eb70986103
Roylouis-web <powellleroy488@gmail.com>
2022-09-15 19:34:58 +0000

    My ninth task

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - prints a square, followed by a new line.
+ * @size: integer parameter to be used in the in thr function
+ * Return: returns an integer
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (size > 0)
+				_putchar('#');
+			else
+				_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
