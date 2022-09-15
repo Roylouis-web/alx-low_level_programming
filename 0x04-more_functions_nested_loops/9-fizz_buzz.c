0370a21711fd8d77554a65a3ab097f770b4e434e
Roylouis-web <powellleroy488@gmail.com>
2022-09-15 22:11:33 +0000

    My tenth task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,36 +4,40 @@
  *Return: Always 0 Successful
  */
 int main(void)
-{int i;
+{
+	int i;
+
 for (i = 0; i <= 100; i++)
 {
 if (i > 9 && i % 3 != 0 && i % 5 != 0)
 {
-	printf("%d", i / 10);
-	printf("%d", i % 10);
-	printf(" ");
+printf("%d", i / 10);
+printf("%d", i % 10);
+printf(" ");
 }
 else if (i <= 9 && i % 3 != 0 && i % 5 != 0)
 {
-	printf("%d", i);
-	printf(" ");
+printf("%d", i);
+printf(" ");
 }
 else if (i % 3 == 0 && i % 5 == 0 && i != 0)
 {
-	printf("FizzBuzz");
-	printf(" ");
+printf("FizzBuzz");
+printf(" ");
 }
 else if (i % 3 == 0 && i != 0)
-	printf("Fizz");
-	printf(" ");
+{
+printf("Fizz");
+printf(" ");
 }
 else if (i % 5 == 0 && i != 0)
 {
-	printf("Buzz");
-	printf(" ");
+printf("Buzz");
+printf(" ");
 }
 }
 printf("\n");
 return (0);
 }
+
 
