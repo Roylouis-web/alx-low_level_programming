8aea4aa7ddaa90b6b9bb520c6d831f1687c8d9ed
Roylouis-web <powellleroy488@gmail.com>
2022-09-16 01:20:43 +0000

    My tenth gtask

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,33 +5,32 @@
  */
 int main(void)
 {
-	int i;
+	int x;
 
-for (i = 0; i <= 100; i++)
-{
-if (i > 9 && i % 3 != 0 && i % 5 != 0)
-{
-printf(" %d", i);
-}
-else if (i <= 9 && i % 3 != 0 && i % 5 != 0)
-{
-printf(" %d ", i);
-}
-else if (i % 3 == 0 && i % 5 == 0 && i != 0)
-{
-printf(" FizzBuzz ");
-
-}
-else if (i % 3 == 0 && i != 0)
-{
-printf(" Fizz ");
-}
-else if (i % 5 == 0 && i != 0)
-{
-printf(" Buzz ");
-}
-}
-printf("\n");
-return (0);
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", x);
+		}
+		if (x != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
 }
 
