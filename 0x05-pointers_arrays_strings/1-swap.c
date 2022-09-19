03b29bb995dd03f0791d9b9a87bece4271d1dac0
Roylouis-web <powellleroy488@gmail.com>
2022-09-19 09:51:38 +0000

    My second task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+/**
+ * swap_int -  function that swaps the values of two integers.
+ * @a: integer pointer parameter
+ * @b: integer pointer parameter
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
