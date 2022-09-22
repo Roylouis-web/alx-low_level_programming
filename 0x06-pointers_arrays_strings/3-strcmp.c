c6850dd6fb7aae0c54f2828cbde68b1a9196877d
Roylouis-web <powellleroy488@gmail.com>
2022-09-22 08:02:34 +0000

    My fourth task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,35 +7,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int a = 0, b = 0, c = 0, r = 0, lim;
+	int i, flag = 0;
 
-	while (s1[a])
+	for (i = 0; s1[i] || s2[i] != '\0'; i++)
 	{
-		a++;
+		if (s1[i] != s2[i] && s1[i] < s2[i])
+			flag = -15;
+		else if (s1[i] != s2[i] && s1[i] > s2[i])
+			flag = 15;
+		else if (s1[i] == s2[i])
+			flag = 0;
 	}
 
-	while (s2[b])
-	{
-		b++;
-	}
-	if (a <= b)
-		lim = a;
-	else
-		lim = b;
-
-	while (c <= lim)
-	{
-		if (s1[c] == s2[c])
-		{
-			c++;
-			continue;
-		}
-		else
-		{
-			r = s1[c] - s2[c];
-		}
-
-		c++;
-	}
-	return (r);
+	return (flag);
 }
