87bda013142a97d407dbc25e98e0f588278c4221
Roylouis-web <powellleroy488@gmail.com>
2022-09-22 09:58:17 +0000

    My eigth task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,24 +6,16 @@
  */
 char *leet(char *s)
 {
-	int i;
-
+	int i = 0, k;
+	char s0[] = "aAeEoOtTlL";
+	char s1[] = "4433007711";
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-
-		else if (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-
-		else if (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-
-		else if (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-
-		else if (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (k = 0; k <= 9; k++)
+		{
+			if(s0[k] == s[i])
+				s[i] = s1[k];
+		}
 	}
 
 	return (s);
