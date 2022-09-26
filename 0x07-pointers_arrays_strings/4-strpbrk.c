d40997a74f53e96b5d686c22907020da208f8331
Roylouis-web <powellleroy488@gmail.com>
2022-09-26 14:18:44 +0000

    My fifth task

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,16 +6,16 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
+
 	int i, j;
 
-	for (i = 0; accept[i]; i++)
+	for (i = 0; accept[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j]; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (accept[j] == s[i])
 				return (s + i);
 		}
 	}
-
 	return (0);
 }
