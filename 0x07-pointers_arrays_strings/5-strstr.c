88cee856493265b534814766025dfa1b4a0ea38d
Roylouis-web <powellleroy488@gmail.com>
2022-09-26 12:00:18 +0000

    My sixth task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,13 +8,20 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
+	if (needle[0] == '\0')
+		return (haystack);
+
 	for (i = 0; haystack[i]; i++)
 	{
 		for (j = 0; needle[j]; j++)
 		{
-			if (needle[j] == haystack[i])
-				return (needle);
+			if (haystack[i + j] != needle[j])
+				break;
+			if (needle[i + j] == '\0')
+				return (haystack + i);
 		}
 	}
+
+	return ('\0');
 
 }
