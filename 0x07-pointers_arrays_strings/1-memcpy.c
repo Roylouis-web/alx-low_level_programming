337a16b3960d17aea7fe9ba237b989a5524db166
Roylouis-web <powellleroy488@gmail.com>
2022-09-26 08:59:36 +0000

    My second task

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: character pointer parameter to be used
+ * @src: character pointer parameter to be used
+ * @n: integer parameter to be used
+ * Return: a character pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = dest[i]; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
