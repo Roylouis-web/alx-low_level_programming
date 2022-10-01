0a502d3fd3540194f10c0155726b9415680638e9
Roylouis-web <powellleroy488@gmail.com>
2022-10-01 14:27:53 +0000

    My last task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,31 +8,30 @@
  */
 int main(int argc, char **argv)
 {
-	int n, coins = 0;
+	int total, change;
 
-	if (argc != 2)
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	if (argv[1][0] == '-')
+	change = atoi(argv[1]);
+
+	for (total = 0; change > 0; total++)
 	{
-		printf("0\n");
+		if (change - 25 >= 0)
+			change = change - 25;
+		else if (change - 10 >= 0)
+			change = change - 10;
+		else if (change - 5 >= 0)
+			change = change - 5;
+		else if (change - 2 >= 0)
+			change = change - 2;
+		else if (change - 1 >= 0)
+			change = change - 1;
 	}
-
-	n = atoi(argv[1]);
-
-	coins += n / 25;
-	n = n % 25;
-	n = n % 10;
-	coins += n / 5;
-	n = n % 5;
-	coins += n / 2;
-	n = n % 2;
-	coins += n / 1;
-
-	printf("%d\n", coins);
+	printf("%d\n", total);
 
 	return (0);
 
