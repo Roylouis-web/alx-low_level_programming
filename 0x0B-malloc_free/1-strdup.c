fba056d746cd342827b71ed21f66cb5b448ce8a8
Roylouis-web <powellleroy488@gmail.com>
2022-10-03 09:18:23 +0000

    My second task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - eturns a pointer to a newly allocated space in memory, 
+ * which contains a copy of the string given as a parameter.
+ * @str: character pointer parameter
+ * Return: a character pointer
+ */
+char *_strdup(char *str)
+{
+	unsigned int i;
+	unsigned int len = strlen(str);
+	char *ptr;
+
+	ptr = malloc(len * sizeof(char));
+
+	while (i < len)
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+
+	return (ptr);
+
+}
