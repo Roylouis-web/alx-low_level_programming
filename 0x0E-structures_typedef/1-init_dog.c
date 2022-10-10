ad94b33e17b45a651953d71488a08b0584cebb93
Roylouis-web <powellleroy488@gmail.com>
2022-10-10 12:00:43 +0000

    My second task

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 #include "dog.h"
 /**
  * init_dog - function that initialize a variable of type
- * @dog: character pointer parameter
+ * @d: character pointer parameter
  * @name: character pointer parameter
  * @age: float parameter
  * @owner: character pointer parameter
@@ -12,8 +12,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		d ->name = name;
-		d ->age = age;
-		d ->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
