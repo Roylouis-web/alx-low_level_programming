1205b914acc23cfefd2dd3b7e2958c774c435b58
Roylouis-web <powellleroy488@gmail.com>
2022-10-10 11:19:42 +0000

    My first task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+* struct dog - Dog attributes
+* @name: The name of the dog.
+* @age: The age of the dog.
+* @owner: The owner of the dog.
+*
+* Description: The attributes of a dog.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
