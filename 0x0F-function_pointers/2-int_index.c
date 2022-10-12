fe2924ba2e567a27924f0d6e1d2cbf82855896d9
Roylouis-web <powellleroy488@gmail.com>
2022-10-12 08:50:09 +0000

    My third task

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+/**
+ * int_index - function that searches for an integer.
+ * @array: integer pointer parameter
+ * @cmp: function pointer parameter
+ * @size: integer parameter
+ * Return: an integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+				i++;
+			}
+		}
+	}
+
+	return (-1);
+}
