9ca5a49c2212f430afe32a83dcc3eb89f59beea0
Roylouis-web <powellleroy488@gmail.com>
2022-10-12 08:15:55 +0000

    My first task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - function that prints a name.
+ * @name: character pointer parameter
+ * @f: function pointer parameter
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return (NULL);
+	f(name);
+}
