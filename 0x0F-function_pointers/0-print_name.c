a736afd080955d281d3f623ef62bd5b080d91ca0
Roylouis-web <powellleroy488@gmail.com>
2022-10-12 11:17:52 +0000

    My first task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include "function_pointers.h"
 /**
  * print_name - function that prints a name.
  * @name: character pointer parameter
@@ -7,5 +8,5 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-	f(name);
+		f(name);
 }
