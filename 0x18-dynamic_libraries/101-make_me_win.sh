e68761c755e3d09aa4d652a1b7d78f1e21d9ad4b
Roylouis-web <powellleroy488@gmail.com>
2022-12-12 19:29:37 +0000

    my final task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/libinjection.so https://github.com/BennettDixon/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libinjection.so
+wget -O /tmp/libinjection.so https://github.com/Roylouis-web/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libinjection.so
 export LD_PRELOAD=/tmp/libinjection.so
